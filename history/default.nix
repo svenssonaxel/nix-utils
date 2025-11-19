bb71dc7fed5432a7c8256ab5c4a83021c3e888a2
Axel Svensson <mail@axelsvensson.com>
2025-11-19 08:17:40 +0100

    history.ghostscript

--- history/default.nix
+++ history/default.nix
@@ -2,6 +2,8 @@
 }:
 
 rec {
+  ghostscript = import ./ghostscript.nix {
+    inherit system; history_nixpkgs = nixpkgs; };
   nixpkgs = import ./nixpkgs.nix {
     inherit system; };
   poppler-utils = import ./poppler-utils.nix {
---
+++ history/ghostscript.nix
@@ -0,0 +1,23 @@
+{ system
+, history_nixpkgs
+}:
+
+let
+  inherit (history_nixpkgs.latest.pkgs.lib) recursiveUpdate;
+  gs = pkg: recursiveUpdate pkg { meta.mainProgram = "gs"; };
+in {
+  v9_15    = gs history_nixpkgs.v15_09.pkgs.ghostscript // { version = "9.15"; };
+  v9_18    = gs history_nixpkgs.v16_09.pkgs.ghostscript;
+  v9_20    = gs history_nixpkgs.v17_09.pkgs.ghostscript;
+  v9_22    = gs history_nixpkgs.v18_03.pkgs.ghostscript;
+  v9_24    = gs history_nixpkgs.v18_09.pkgs.ghostscript;
+  v9_26    = gs history_nixpkgs.v19_09.pkgs.ghostscript;
+  v9_50    = gs history_nixpkgs.v20_03.pkgs.ghostscript;
+  v9_52    = gs history_nixpkgs.v20_09.pkgs.ghostscript;
+  v9_53_3  = history_nixpkgs.v21_11.pkgs.ghostscript;
+  v9_56_1  = history_nixpkgs.v22_11.pkgs.ghostscript;
+  v10_01_1 = history_nixpkgs.v23_05.pkgs.ghostscript;
+  v10_02_1 = history_nixpkgs.v24_05.pkgs.ghostscript;
+  v10_04_0 = history_nixpkgs.v24_11.pkgs.ghostscript;
+  v10_05_1 = history_nixpkgs.v25_05.pkgs.ghostscript;
+}
